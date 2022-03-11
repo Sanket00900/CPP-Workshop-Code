7a81624104291a7a3adcd0679ea70b6e68a2bab9
Sanket Sanjay Jadgale <sanketsjagdale999@gmail.com>
2022-03-11 09:43:42 +0530

    Update ptraddition.cpp

--- .vscode/ptraddition.cpp
+++ .vscode/ptraddition.cpp
@@ -7,12 +7,12 @@ int main()
     int N = 4;
     int *ptr2 = &N;
 
-    cout << "Pointer ptr2 before Addition: ";
+    cout << "Pointer ptr2 before Addition: "; // 1000
     cout << ptr2 << endl;
 
     // Addition of 4 to ptr2
     ptr2 = ptr2 + 4;
-    cout << "Pointer ptr2 after Addition: ";
+    cout << "Pointer ptr2 after Addition: "; // 1016
     cout << ptr2 << endl;
 
     return 0;
